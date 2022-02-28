8f9973002e196a0a0cfaab1d69ad2df9b1832aec
Muhammad Hassan <muhammadhassan518@gmail.com>
2022-02-28 10:28:53 +0500

    Update ViewController.swift

--- FetchDataInTableView/ViewController.swift
+++ FetchDataInTableView/ViewController.swift
@@ -26,29 +26,27 @@ class ViewController: UIViewController {
         
         TableViewS.delegate = self
         TableViewS.dataSource = self
-        
-        
-    }
+        }
+    
+    
     func GetData()
     {
         let url = URL(string: "https://missingpersonapps.000webhostapp.com/fetch_user_data2.php")
         URLSession.shared.dataTask(with: url!) { (Data,response,error) in
             do
             {  if error == nil
-                {  self.ListOfMissingPerson = try
+                {
+                self.ListOfMissingPerson = try
                 JSONDecoder().decode([jsonData].self, from: Data!)
             
                 for _ in self.ListOfMissingPerson
-                {
-                    
-                    DispatchQueue.main.async {
-                        self.TableViewS.reloadData()
-                    }
-                }
+                {  DispatchQueue.main.async {  self.TableViewS.reloadData() } }
+                
             }
           }
             catch
             { print("error in loadind json data") }
+            
         }.resume()
     }
 
@@ -56,21 +54,16 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource
 {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        1
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        ListOfMissingPerson.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+    func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { ListOfMissingPerson.count  }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
         let cell = TableViewS.dequeueReusableCell(withIdentifier: "CustomCellTableViewCell", for: indexPath) as! CustomCellTableViewCell
         cell.lblName.text = "Name: \(ListOfMissingPerson[indexPath.row].name)"
         cell.lblFatherName.text = "F.Name: \(ListOfMissingPerson[indexPath.row].fathername)"
-        
         return cell
     }
-    
-    
 }
 
